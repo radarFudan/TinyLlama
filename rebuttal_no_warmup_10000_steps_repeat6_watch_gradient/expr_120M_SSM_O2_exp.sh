c8a83d4c341e75783d27bb72467659cea29f91b1
Malay <25528016+radarFudan@users.noreply.github.com>
2023-11-13 15:07:55 +0800

    add watch gradient and wandb logging to the code

---
+++ rebuttal_no_warmup_10000_steps_repeat6_watch_gradient/expr_120M_SSM_O2_exp.sh
@@ -0,0 +1,6 @@
+lightning run model \
+    --node-rank=0  \
+    --accelerator=cuda \
+    --devices=1 \
+    --num-nodes=1 \
+    ./rebuttal_no_warmup_10000_steps_repeat6_watch_gradient/tinyllama_120M_SSM_O2_exp.py --devices 1 --train_data_dir /home/aiops/wangsd/TinyLlama/data/mix_sample_combined
