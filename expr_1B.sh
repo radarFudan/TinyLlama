abd051a42889b9518b751cbb8057824148806a20
Malay <25528016+radarFudan@users.noreply.github.com>
2023-11-16 14:49:25 +0800

    1B baseline also runnign

--- expr_1B.sh
+++
@@ -1,6 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 lightning run model \
-    --node-rank=0  \
-    --accelerator=cuda \
-    --devices=1 \
-    --num-nodes=1 \
-    pretrain/tinyllama_WK1B.py --devices 1  --train_data_dir data/wikitext  --val_data_dir data/wikitext --use_wikitext True
